f6e43c6da93cb508216db9fb1c488f49427cc167
Ana Carolina <cscarol20@gmail.com>
2020-11-28 18:34:52 -0300

    Tutorial 19 - Firebase functions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import cartReducer from './cart/cart.reducer';
 import productsReducer from './products/products.reducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+export const rootReducer = combineReducers({
     user: userReducer,
     productsData: productsReducer,
     cartData: cartReducer,
 });
+
+const configStorage = {
+    key: 'root',
+    storage,
+    whitelist: ['cartData']
+};
+
+export default persistReducer(configStorage, rootReducer);
